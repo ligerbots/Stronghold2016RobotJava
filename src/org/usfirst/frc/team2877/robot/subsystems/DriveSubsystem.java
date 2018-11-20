13b2d5f8e97f69a864078ab8a11715eb3baa0a38
matthewdleonard <matthewdleonard01@gmail.com>
2018-11-19 20:30:54 -0500

    Added basics of drive subsystem
    We will fill in the details later; I need to look at the code again

--- src/org/usfirst/frc/team2877/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team2877/robot/subsystems/DriveSubsystem.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.team2877.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  *
@@ -9,12 +14,39 @@ public class DriveSubsystem extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	WPI_TalonSRX leftLeader;
+	WPI_TalonSRX leftFollower;
+	WPI_TalonSRX rightLeader;
+	WPI_TalonSRX rightFollower;
+	//The rest of the talon declarations
+
+	DifferentialDrive robotDrive;
+	PIDController turningController;
+	
+	int[] talons;
+	
+	
 	private class Position {
 		double X, Y;
 		double Angle;
 	}
+	
 	Position m_pos;
 	double m_originalPosition;
+	public DriveSubsystem() {
+	//initialization
+		leftLeader.set(ControlMode.PercentOutput, 0.0);
+		leftFollower.set(ControlMode.Follower, 0);
+		rightLeader.set(ControlMode.PercentOutput, 0.0);
+		rightFollower.set(ControlMode.Follower, 2);
+		
+		robotDrive = new DifferentialDrive(leftLeader, rightLeader);
+	}
+	
+	public void driveForward() {
+		robotDrive.arcadeDrive(0.5, 0);
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
