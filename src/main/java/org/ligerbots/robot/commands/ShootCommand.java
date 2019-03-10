ca43ce0cd6d6e243e54887962128ad5e3b7fb086
Sequal32 <connor0530@gmail.com>
2019-03-10 13:23:50 -0400

    ShootCommand

--- src/main/java/org/ligerbots/robot/commands/ShootCommand.java
+++ src/main/java/org/ligerbots/robot/commands/ShootCommand.java
@@ -1,5 +1,10 @@
 package org.ligerbots.robot.commands;
 
+import org.ligerbots.robot.Robot;
+import org.ligerbots.robot.subsystems.IntakeSubsystem;
+import org.ligerbots.robot.subsystems.ShooterSubsystem;
+import org.ligerbots.robot.subsystems.WedgeSubsystem;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ShootCommand extends Command {
@@ -8,27 +13,49 @@ public class ShootCommand extends Command {
         // eg. requires(chassis);
     }
 
+    WedgeSubsystem wedge;
+    IntakeSubsystem intake;
+    ShooterSubsystem shooter;
+
+    boolean done = false;
+    int ticks = 0;
+
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        wedge = Robot.wedge;
+        intake = Robot.intake;
+        shooter = Robot.shooter;
 
+        if (!intake.IntakeReadyToFire())
+            done = true;
+        else {
+            //TODO - LEDS for shooting animation
+        }
+
+        setInterruptible(false);
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-
+        ticks++;
+        if (ticks == 1)
+            shooter.Fire();
+        else if (ticks == 25)
+            done = true;
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-      return false;
+      return done;
     }
   
     // Called once after isFinished returns true
     @Override
     protected void end() {
+        shooter.Retract();
     }
   
     // Called when another command which requires one or more of the same
