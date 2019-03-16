e5377d7cb35370ea77577bc1929e973600695fc9
Sequal32 <connor0530@gmail.com>
2019-03-15 21:46:40 -0400

    LED animation for shooting

--- src/main/java/org/ligerbots/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/org/ligerbots/robot/subsystems/ShooterSubsystem.java
@@ -1,6 +1,8 @@
 package org.ligerbots.robot.Subsystems;
 
+import org.ligerbots.robot.Robot;
 import org.ligerbots.robot.RobotMap;
+import org.ligerbots.robot.Subsystems.LEDSubsystem.LEDState;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -9,12 +11,15 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ShooterSubsystem extends Subsystem {
 
     DoubleSolenoid shooterSolenoid;
+    LEDSubsystem leds;
 
     public ShooterSubsystem() {
+        leds = Robot.leds;
         shooterSolenoid = new DoubleSolenoid(RobotMap.PCM_CAN, RobotMap.PCM_SHOOTER_FIRE, RobotMap.PCM_SHOOTER_RETRACT);
     }
 
     public void Fire() {
+        leds.SetLEDs(LEDState.SHOOT);
         shooterSolenoid.set(Value.kForward);
     }
 
